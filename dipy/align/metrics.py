64c57348a67fb2e5f7609711b6c4fa624a9d34da
TomasGuija <tomasguija@gmail.com>
2026-07-28 17:56:36 +0200

    Revert "NF: MI implementation for SyN registration"
    This reverts commit 34b30c881304827eb6caea1b2f1b6d24e1d21086.

--- dipy/align/metrics.py
+++ dipy/align/metrics.py
@@ -274,7 +274,7 @@ class MIMetric(SimilarityMetric):
             self.backward_displacement = np.empty(displacement_shape, dtype=dtype)
 
         self.gradient_moving = np.empty(
-            shape=self.moving_image.shape + (self.dim,), dtype=np.float32
+            shape=self.moving_image.shape + (self.dim,), dtype=dtype
         )
         for i, grad in enumerate(gradient(self.moving_image)):
             self.gradient_moving[..., i] = grad
@@ -286,7 +286,7 @@ class MIMetric(SimilarityMetric):
             self.reorient_vector_field(self.gradient_moving, self.moving_direction)
 
         self.gradient_static = np.empty(
-            shape=self.static_image.shape + (self.dim,), dtype=np.float32
+            shape=self.static_image.shape + (self.dim,), dtype=dtype
         )
         for i, grad in enumerate(gradient(self.static_image)):
             self.gradient_static[..., i] = grad
